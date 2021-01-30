e3279cebf54b6df98b33cf4f9ec7faaff5bce405
Pojemnik <andriej194@gmail.com>
2021-01-30 16:38:23 +0100

    Added player movement and readme

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float xBound;
+    public float defaultY;
+
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if ((moveHorizontal > 0 && rb2d.position.x < xBound) || (moveHorizontal < 0 && rb2d.position.x > -xBound))
+        {
+            rb2d.MovePosition(new Vector2(rb2d.position.x + (moveHorizontal * speed * Time.fixedDeltaTime), defaultY));
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Out of space Unity edition
+
+The ultimate goal of this project is to learn Unity by porting some of the older games originaly written in C++ with SFML library.
+
+After poring all of the features of original version, I will try to improve the game a bit by adding more levels, sounds etc. 
+
+### Link to the original project:
+https://github.com/IIaMoCLitTMW/OUT-OF-SPACE
