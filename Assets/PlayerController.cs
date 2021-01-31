1a8e573696afb7e29a2479968b3467d296957038
Pojemnik <andriej194@gmail.com>
2021-01-31 18:13:09 +0100

    Player can now shoot

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
     public Vector2 bounds;
     public GameObject projectilePrefab;
     public float projectileSpeed;
+    public float shootCooldown;
 
+    private float shootTimer = 0.0f;
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
@@ -18,12 +20,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && shootTimer <= 0.0f)
+        {
+            GameObject newProjectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+            Rigidbody2D projectileRB = newProjectile.GetComponent<Rigidbody2D>();
+            projectileRB.velocity = new Vector2(0, projectileSpeed);
+            shootTimer += shootCooldown;
+        }
     }
     void FixedUpdate()
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         Vector2 move = rb2d.position;
+        if (shootTimer >= 0.0f)
+        {
+            shootTimer -= Time.fixedDeltaTime;
+        }
 
         if ((moveInput.x > 0 && rb2d.position.x < bounds.x) || (moveInput.x < 0 && rb2d.position.x > -bounds.x))
         {
---
+++ Assets/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float upperBound;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb2d.position.y >= upperBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
