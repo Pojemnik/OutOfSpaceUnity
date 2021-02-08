4d6aba11896fc7b4cd25f60f0826100ec84edcf4
Pojemnik <andriej194@gmail.com>
2021-02-08 22:40:05 +0100

    Added main menu

---
+++ Assets/MenuMenager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMenager : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void OnCreditsClicked()
+    {
+
+    }
+
+    public void OnExitClicked()
+    {
+        Application.Quit();
+    }
+}
