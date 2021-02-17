fa41f0cf822398bb4e2335d1ecf74b8f58faff43
Pojemnik <andriej194@gmail.com>
2021-02-17 13:27:38 +0100

    Added cursor in main menu

---
+++ Assets/scripts/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject cursor;
+
+    private CursorController cursorController;
+
+    private void Awake()
+    {
+        cursorController = cursor.GetComponent<CursorController>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter");
+        cursorController.OnPointerButtonEnter(gameObject);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit");
+        cursorController.OnPointerButtonExit(gameObject);
+    }
+}
---
+++ Assets/scripts/CursorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorController : MonoBehaviour
+{
+    public float yOffset;
+    public float widthOffset;
+
+    private RectTransform rectTransform;
+    private Image leftCursorImage;
+    private Image rightCursorImage;
+
+    private void Awake()
+    {
+        rectTransform = transform as RectTransform;
+        RectTransform[] children = GetComponentsInChildren<RectTransform>();
+        foreach (RectTransform rect in children)
+        {
+            if (rect.name == "CursorLeft")
+            {
+                leftCursorImage = rect.gameObject.GetComponent<Image>();
+            }
+            if (rect.name == "CursorRight")
+            {
+                rightCursorImage = rect.gameObject.GetComponent<Image>();
+            }
+        }
+    }
+
+    public void OnPointerButtonEnter(GameObject button)
+    {
+        leftCursorImage.color = Color.white;
+        rightCursorImage.color = Color.white;
+        RectTransform buttonTransform = button.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = buttonTransform.anchoredPosition + new Vector2(0, yOffset);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, buttonTransform.rect.width + widthOffset);
+    }
+
+    public void OnPointerButtonExit(GameObject button)
+    {
+        leftCursorImage.color = Color.clear;
+        rightCursorImage.color = Color.clear;
+    }
+}
