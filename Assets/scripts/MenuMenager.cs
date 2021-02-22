2cb6c7f9e618f6eb1b552d295a376b58d37677b6
Pojemnik <andriej194@gmail.com>
2021-02-22 21:38:14 +0100

    Added sound settings in main menu

--- Assets/scripts/MenuMenager.cs
+++ Assets/scripts/MenuMenager.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MenuMenager : MonoBehaviour
 {
     public GameObject menuCanvas;
     public GameObject loadingCnavas;
     public GameObject creditsCnavas;
+    public GameObject settingsCanvas;
+    public AudioMixer mixer;
     public UnityEngine.UI.Slider loadingBar;
+
+    private readonly string[] prefsNames = { "MasterVolume", "SoundsVolume", "MusicVolume" };
+
+    private void Start()
+    {
+        foreach(string name in prefsNames)
+        {
+            if (PlayerPrefs.HasKey(name))
+            {
+                float value = PlayerPrefs.GetFloat(name);
+                print(value);
+                mixer.SetFloat(name, value);
+            }
+        }
+    }
 
     public void OnPlayClicked()
     {
         menuCanvas.SetActive(false);
+        settingsCanvas.SetActive(false);
         loadingCnavas.SetActive(true);
         StartCoroutine(LoadGameScene());
     }
@@ -27,16 +46,26 @@ public class MenuMenager : MonoBehaviour
         }
     }
 
+    public void OnSettingsClicked()
+    {
+        menuCanvas.SetActive(false);
+        settingsCanvas.SetActive(true);
+        creditsCnavas.SetActive(false);
+    }
+
     public void OnCreditsClicked()
     {
         menuCanvas.SetActive(false);
+        settingsCanvas.SetActive(false);
         creditsCnavas.SetActive(true);
     }
 
     public void OnBackClicked()
     {
         menuCanvas.SetActive(true);
+        settingsCanvas.SetActive(false);
         creditsCnavas.SetActive(false);
+        PlayerPrefs.Save();
     }
 
     public void OnExitClicked()
---
+++ Assets/scripts/SettingsController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsController : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public UnityEngine.UI.Slider generalSlider;
+    public UnityEngine.UI.Slider musicSlider;
+    public UnityEngine.UI.Slider soundsSlider;
+
+    private const string sounds = "SoundsVolume";
+    private const string master = "MasterVolume";
+    private const string music = "MusicVolume";
+
+    void OnEnable()
+    {
+        float temp;
+        mixer.GetFloat(master, out temp);
+        generalSlider.value = GetSliderValue(temp);
+        mixer.GetFloat(sounds, out temp);
+        soundsSlider.value = GetSliderValue(temp);
+        mixer.GetFloat(music, out temp);
+        musicSlider.value = GetSliderValue(temp);
+    }
+
+    public void OnGeneralSliderValueChange(float value)
+    {
+        ApplySliderValue(value, master);
+    }
+
+    public void OnSoundsSliderValueChange(float value)
+    {
+        ApplySliderValue(value, sounds);
+    }
+
+    public void OnMusicSliderValueChange(float value)
+    {
+        ApplySliderValue(value, music);
+    }
+
+    private void ApplySliderValue(float value, string name)
+    {
+        float volume = GetVolume(value);
+        mixer.SetFloat(name, volume);
+        PlayerPrefs.SetFloat(name, volume);
+    }
+
+    private float GetSliderValue(float volume)
+    {
+        return Mathf.Pow(10, Mathf.Clamp(volume, -80, 20) / 20);
+    }
+
+    private float GetVolume(float sliderValue)
+    {
+        return Mathf.Log10(Mathf.Clamp(sliderValue, 0.0001f, 10f)) * 20;
+    }
+}
